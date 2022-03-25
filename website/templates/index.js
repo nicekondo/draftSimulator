e3a263d2da74dd7dc6d399c4b0a1faf820c3566b
nicekondo <onigiri413@icloud.com>
2022-03-25 13:30:36 +0000

    テストコード追加

--- website/templates/index.js
+++ website/templates/index.js
@@ -1,6 +1,9 @@
+console.log("起動テスト");
+
 document.getElementById("test").addEventListener("click", function() {
     document.getElementById("test").textContent="決定が押されました";
     alert("決定が押されました");
+    console.log("決定が押されました");
 });
 
 
